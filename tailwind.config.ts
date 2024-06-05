9675c4da81c2d5d80c0cfbde3ab1a7772c724028
JalilaAbd <abdessamadjalila@gmail.com>
2024-06-05 08:39:28 +0100

    update page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,11 +11,6 @@ const config: Config = {
       fontFamily: {
         montserrat: ["Montserrat"],
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [],
